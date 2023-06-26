8f7b9152e6d6aff8f8cdf96778a2f1d0f267d968
Haripriya. C <110826217+Priyahari123@users.noreply.github.com>
2023-06-26 17:45:45 +0530

    Add files via upload

--- firstpro/My_Application/models.py
+++ firstpro/My_Application/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class info(models.Model):
-    name=models.CharField(max_length=20, default="")
+    username=models.CharField(max_length=20, default="")
     email=models.CharField(max_length=50, default="")
+    password=models.CharField(max_length=15,default="")
+    re_password=models.CharField(max_length=15,default="")
     address=models.CharField(max_length=30,default="")
--- firstpro/My_Application/views.py
+++ firstpro/My_Application/views.py
@@ -1,24 +1,47 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.models import User, auth
 from django.http import HttpResponse
 from .models import info
+from django.contrib import messages 
+
+
+
+
 
 # Create your views here.
 def home(request):
     data=info.objects.all()
+    
     if(data!=''):
         return render(request,"home.html",{'datas':data})
     else:
         return render(request,'home.html')
 def read(request):
     if request.method=='POST':
-        name=request.POST['Name']
+        username=request.POST['Name']
         email=request.POST['Email']
+        password=request.POST['password']
+        re_password=request.POST['re_password']
         address=request.POST['Address']
+       
+       
+       
         obj=info()
-        obj.name=name
+        obj.username=username
         obj.email=email
+        obj.password=password
+        obj.re_password=re_password
         obj.address=address
+        if info.objects.filter(username=username).exists():
+            return HttpResponse(username+" Already Exists!!!!!.")    
+       
         obj.save()
         data=info.objects.all()
-        return redirect('home')
+        return redirect("home")
+        
     return render(request,"home.html")
+
+
+
+
+
