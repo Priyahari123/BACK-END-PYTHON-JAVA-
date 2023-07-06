2b7da4412413fd46894433dcf205a283950e20ab
Haripriya. C <110826217+Priyahari123@users.noreply.github.com>
2023-07-06 11:03:14 +0530

    Add files via upload

--- register/myapp/models.py
+++ register/myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Addprofile(models.Model):
+    Name = models.CharField(max_length=20)  
+    email_id = models.CharField(max_length=30) 
+    password = models.CharField(max_length=40) 
+    password1 = models.CharField(max_length=30) 
--- register/myapp/views.py
+++ register/myapp/views.py
@@ -3,12 +3,14 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import logout
+from .models import Addprofile
 
 
 
 # Create your views here.
 def home(request):
-    return render(request,"home.html")
+    data=Addprofile.objects.all()
+    return render(request,"home.html",{'data':data})
 
 
 def register(request):
@@ -17,25 +19,35 @@ def register(request):
         email_id=request.POST['email_id']
         password=request.POST['password']
         password1=request.POST['password1']
+
+        obj=Addprofile()
+        obj.Name=Name
+        obj.email_id=email_id
+        obj.password=password
+        obj.password1=password1
         if password == password1:
-            user=User.objects.create_user(username=Name,email=email_id,password=password1)
-            user.is_staff = True
-            user.is_superuser =True
-         
-            user.save()
+            if Addprofile.objects.filter(Name=Name).exists():
+                
+                messages.warning(request,'Username already exists !!!')
+                return redirect("login")
+            obj.is_staff=True
+            obj.is_superuser =True
+            obj.save()
+            data=Addprofile.objects.all()
             messages.success(request,'Your account hasbeen succesfully created!!!')
             return redirect('login')
-            
         else:
             messages.warning(request,'Password Mismatching!!!')
             return redirect('register')
-            
+           
+       
 
 
     return render(request,"register.html")
 
 def profile(request):
     return render(request,"profile.html")
+    
 
 
 
